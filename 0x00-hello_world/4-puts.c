9788826a080600905b4a9cc9bc47014e479797f9
Jason13242 <jasonruiz@ufm.edu>
2022-02-06 15:43:39 -0600

    subiendo el ejercicio 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-
+/*
+*This code prints a string and returns 0
+*as per de the instructions provided 
+*this code was done betty style
+*/
 int main(void)
- {
-   puts("\"Programming is like building a multilingual puzzle");
-
-   return (0);
- }
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
