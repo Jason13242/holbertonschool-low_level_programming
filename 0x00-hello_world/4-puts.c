92dcdba37fbc14ed74f2e980e6f2a09c49d80718
Jason13242 <jasonruiz@ufm.edu>
2022-02-08 12:09:49 -0600

    subiendo con betty style este file completo ahora que ya modifique mi emacs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,18 @@
+/**
+ * File: 4-print_alphabt.c
+ * Auth: Jason Ruiz
+ */
+
 #include<stdio.h>
-/*
-*This code prints a string and returns 0
-*as per de the instructions provided 
-*this code was done betty style
-*/
+
+/**
+ * main - Print all the letters except q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
+/**
+ *File: 5-printf.c
+ *Auth: Jason Ruiz                                                            
+ */
 #include<stdio.h>
-/* 
- *this program prints the required output with printf
- *this program compiles withouth waring the using -Wall gcc
+/**
+ * main - printf test
+ * Return: 0
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
