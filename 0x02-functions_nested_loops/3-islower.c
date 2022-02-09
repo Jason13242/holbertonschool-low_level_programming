ffd7e84a5d29bc14fcc9091ce59eefec0e95975a
Jason13242 <jasonruiz@ufm.edu>
2022-02-09 11:51:58 -0600

    subiendo el tres

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+/*
+ * File: 3-islower.c
+ * Auth: Jason Ruiz
+ */
+
+#include"main.h"
+
+/**
+ *  _islower - function that prints 
+ *             that checks if a char
+ *             is upper or lowecase
+ * Return: 1 if c lowercase, 0 if upper.
+ */
+
+int _islower(int)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
