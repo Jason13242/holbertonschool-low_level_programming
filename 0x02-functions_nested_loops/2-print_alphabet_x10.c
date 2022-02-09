fe82af5fc46d2e363449e923ff7adc7d1355ce33
Jason13242 <jasonruiz@ufm.edu>
2022-02-09 01:41:15 -0600

    subiendo el segundo

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,41 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Jason Ruiz
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this functions
+ *                      prints 10 times
+ *                      the alphabet
+ * Return: 0.
+ */
+
+void print_alphabet_x10(void)
+{
+
+	char l;
+/*
+ * l - is a variable that represents
+ *     each letter to be printed.
+ */
+
+	int n;
+/*
+ * n - this variable is a counter
+ *     that informs how many times
+ *     the alphabet has been printed
+ *     so it prints exactly 10 times.
+ */
+
+	while (n <= 9)
+	{
+		for (l = 'a' ; l <= 'z'; l++)
+		{
+			_putchar(l);
+		}
+		n++;
+		_putchar ('\n');
+	}
+}
