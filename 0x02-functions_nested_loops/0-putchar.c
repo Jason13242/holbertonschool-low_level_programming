67d09d2793c91367672c80fc1ef1a8ced3d9486c
Jason13242 <jasonruiz@ufm.edu>
2022-02-09 00:48:51 -0600

    subiendo los primeros dos

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/*
+ *File: 0-putchar.c
+ *Auth: Jason Ruiz
+ */
+
+#include<stdio.h>
+
+/**
+ * main - program that prints _putchar, followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
