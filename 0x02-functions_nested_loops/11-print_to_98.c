745c7e350a806ab44ffc2f039c3f96a29273e717
Jason13242 <jasonruiz@ufm.edu>
2022-02-10 13:29:29 -0600

    subiendo el 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: Jason Ruiz
+ */
+
+#include"main.h"
+
+/**
+ * print_to_98 - prints all natural numbers
+ *               from n to 98
+ * Returns: 0;
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		int e;
+/*
+ * e - variable used for equivalence.
+ */
+		for (e = n ; e <= 98 ; e++)
+		{
+			printf("%d", e);
+			printf(", ");
+		}
+	}
+	if (n > 98)
+	{
+		int e;
+
+		for (e = n; e > 98; e--)
+		{
+			prinf("%d", e);
+			printf(", ");
+		}
+	}
+	printf("98");
+	printf("\n");
+}
