3b67eb021f6757b34daf9b36229187c9a774b8e9
Jason13242 <jasonruiz@ufm.edu>
2022-02-10 13:01:21 -0600

    subiendo el 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/*
+ * File: 10-add.c
+ * Auth: Jason Ruiz
+ */
+
+#include"main.h"
+
+/**
+ * add - sums two integers
+ * @a: integer to add
+ * @b: integer to add
+ * Return: the result of summing a + b.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
