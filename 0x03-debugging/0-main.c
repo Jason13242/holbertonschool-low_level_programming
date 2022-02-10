9689b1e58906a84a0febffef577fad4a92a1861a
Jason13242 <jasonruiz@ufm.edu>
2022-02-10 15:42:39 -0600

    subiendo el primer ejercicio de debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,16 @@
-/*
- * File: 0-main.c
- * Auth: Jason Ruiz
- */
 
-#include"main.h"
+#include "main.h"
 
 /**
- * main - tests for a function
- *         with zero
- *         to analize if the int is + o -.
- *Return: 0
+ * main - tests function that prints if integer is positive or negative.
+ * Return: 0
  */
 
 int main(void)
 {
-/*
- * i - variable created to test the function
- */
 	int i;
 
 	i = 0;
-
 	postitive_or_negative(i);
 
 	return (0);
