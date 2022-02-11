a9f4401d54007978a38983c6917ba86d932eb27e
Jason13242 <jasonruiz@ufm.edu>
2022-02-11 16:54:28 -0600

    subiendo el 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,40 @@
+/*
+ * File: 7-print_diagonal.c
+ * Auth: Jason Ruiz
+ */
+
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: the character to be printed.
+ * Return: void.
+ */
+
+void print_diagonal(int n)
+{
+	int x, y;
+/*
+ * x - variable for printing spaces.
+ */
+
+/*
+ * y - varible that prints the slash
+ */
+	if ( n <= 0 )
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		for (x = 1; x <= n; x++)
+		{
+			for (y = 1; y < x; y++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
