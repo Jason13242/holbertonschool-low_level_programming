4dcb1ec35f963cb045641543cf0ec4412366aa55
Jason13242 <jasonruiz@ufm.edu>
2022-02-10 20:05:01 -0600

    subiendo el que imprime digitos

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,25 @@
+/*
+ * File: 3-print_numbers.c
+ * Auth: Jason Ruiz
+ */
+
+#include"main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Returns: void
+ */
+
+void print_numbers(void)
+{
+	int n;
+/*
+ * n - i.e. the number to be printed.
+ */
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
