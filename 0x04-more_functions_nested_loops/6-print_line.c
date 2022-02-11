f2338446382ff5b219f1fd1dc200042832e4a7b2
Jason13242 <jasonruiz@ufm.edu>
2022-02-11 13:23:45 -0600

    subiendo el 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,30 @@
+/*
+ * File: 6-print_line.c
+ * Auth: Jason Ruiz
+ */
+
+#include "main.h"
+
+/**
+ * print_line - draws straight line.
+ * @c: character to be printed.
+ * Return: void.
+ */
+
+void print_line(int n)
+{
+
+	if (n > '0')
+	{
+		while (n > '0')
+		{
+			putchar('_');
+			n--;
+		}
+		putchar('\n');
+	}
+	if (n <= '0')
+	{
+		putchar('\n');
+	}
+}
