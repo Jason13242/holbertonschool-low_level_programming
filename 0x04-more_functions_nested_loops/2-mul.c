8e73489d4aca06b584009ba03ccb79ef0581beb3
Jason13242 <jasonruiz@ufm.edu>
2022-02-10 19:48:01 -0600

    subiendo el ejercicio de multiplicacion

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-mul.c
+ * Auth: Jason Ruiz
+ */
+
+#include"main.h"
+
+/**
+ * mul - multiplies two ints
+ * @a: int to be multiplied.
+ * @b: int to be multiplied.
+ * Return: result of multiplication.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
