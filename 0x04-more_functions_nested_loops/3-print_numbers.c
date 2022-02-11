ea702c2434bd508807ff129e08350cf5c1636805
Jason13242 <jasonruiz@ufm.edu>
2022-02-10 20:24:31 -0600

    corrigiendo el 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,11 +3,11 @@
  * Auth: Jason Ruiz
  */
 
-#include"main.h"
+#include "main.h"
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
- * Returns: void
+ * Return: void
  */
 
 void print_numbers(void)
@@ -19,7 +19,7 @@ void print_numbers(void)
 
 	for (n = '0' ; n <= '9' ; n++)
 	{
-		_putchar(n);
+		_putchar(n + '0');
 	}
 	_putchar('\n');
 }
