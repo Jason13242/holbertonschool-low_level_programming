d540ae1f1f515a24bb93f084f8a206bc08849227
Jason13242 <jasonruiz@ufm.edu>
2022-03-01 16:35:16 -0600

    subiendo el ocho con correcciones

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,10 +16,10 @@ void print_array(int *a, int n)
 {
 	int x;
 
-	for (x = 0; x <= n; x++)
+	for (x = 0; x < n; x++)
 	{
 		printf("%d", a[x]);
-		if (!(x == n))
+		if (!(x == (n - 1)))
 		{
 			printf(",");
 			printf(" ");
